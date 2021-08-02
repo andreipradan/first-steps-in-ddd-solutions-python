91071dd597aeae1348a36da94b8c9fc0e483cdff
Andrei Pradan <andrei.pradan@gmail.com>
2021-08-02 18:59:49 +0300

    Exercise 1 - REFACTOR - Use a test fixture

--- tests/a_precharge_decision.py
+++ tests/a_precharge_decision.py
@@ -5,11 +5,12 @@ from src.justice_app import Suspect
 
 
 class TestAPreChargeDecision:
+    def setup(self):
+        self.advice = OffenceAdvice()
+        self.decision = PreChargeDecision()
+        self.suspect = Suspect(CriminalOffence.CUTTING_AWAY_BUOYS_ETC)
+
     def test_should_record_alternative_offence_advice_against_suspects(self):
-        suspect = Suspect(CriminalOffence.CUTTING_AWAY_BUOYS_ETC)
-        decision = PreChargeDecision()
-        advice = OffenceAdvice()
+        self.decision.record_alternative_offensive_advice(self.suspect, self.advice)
 
-        decision.record_alternative_offensive_advice(suspect, advice)
-
-        assert advice == decision.get_offence_advice_for(suspect)
+        assert self.advice == self.decision.get_offence_advice_for(self.suspect)
