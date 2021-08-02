59c8e1760e166399261cb83571d65003e6e8c2fb
Andrei Pradan <andrei.pradan@gmail.com>
2021-08-02 19:12:16 +0300

    Exercise 2 - REFACTOR - use test setup for TesteThePublicProsecutionService

--- tests/the_public_prosecution_service.py
+++ tests/the_public_prosecution_service.py
@@ -7,16 +7,16 @@ from src.justice_app import Suspect
 
 class TestThePublicProsecutionService:
     def setup(self):
+        suspect = Suspect(CriminalOffence.CUTTING_AWAY_BUOYS_ETC)
+        self.pnc_id = PNCId("AN-ID")
+        self.police_investigation = PoliceInvestigation(self.pnc_id, suspect)
         self.the_pps = PublicProsecutionService()
 
     def test_should_create_a_case_when_receiving_a_pcd_request(self):
-        pnc_id = PNCId("AN-ID")
-        suspect = Suspect(CriminalOffence.CUTTING_AWAY_BUOYS_ETC)
-        police_investigation = PoliceInvestigation(pnc_id, suspect)
 
         police_case = self.the_pps.receive_request_for_pre_charge_decision(
-            police_investigation
+            self.police_investigation
         )
 
-        assert pnc_id == police_case.pnc_id
-        assert police_investigation.suspects == police_case.suspects
+        assert self.pnc_id == police_case.pnc_id
+        assert self.police_investigation.suspects == police_case.suspects
